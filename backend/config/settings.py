c68e7c7a668c60245d1ac8e75729b8220f77b1be
ddefin <ddefin@gmail.com>
2026-06-16 08:46:27 +0600

    qwg

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -144,10 +144,9 @@ AUTH_USER_MODEL = 'platform_app.User'
 
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173", 
     "https://elcourse.app",
     "https://www.elcourse.app",
-    # Если тестируешь локально, добавь и этот адрес:
-    "http://localhost:5173", 
 ]
 # Включаем поддержку credentials (куки, авторизация)
 # Разрешаем отправку кук и заголовков авторизации
@@ -216,5 +215,4 @@ CSRF_TRUSTED_ORIGINS = [
     "https://elcourse.app",
     "https://www.elcourse.app",
 ]
-
 APPEND_SLASH = False
